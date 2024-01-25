0821fb7aa1cc115530191420784ee535ed1f9a61
MatasPal <102602807+MatasPal@users.noreply.github.com>
2024-01-25 15:58:04 +0200

    Add files via upload

---
+++ Antras laboratorinis/Pirma dalis/Antra dalis/Program.cs
@@ -0,0 +1,138 @@
+﻿
+//Recursion
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+
+class TreasureDivisionRecursion
+{
+	static int[] values;
+	static bool[] selected;
+	static List<int> firstBrother, secondBrother;
+	static int minDifference = int.MaxValue;
+
+	/// <summary>
+	/// Recursive solution
+	/// </summary>
+	/// <param name="index"></param>
+	/// <param name="sum1"></param>
+	/// <param name="sum2"></param>
+	static void CanDivide(int index, int sum1, int sum2)
+	{
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+
+		if (index == values.Length)
+		{
+			int difference = Math.Abs(sum1 - sum2);
+			if (difference < minDifference)
+			{
+				minDifference = difference;
+				firstBrother.Clear();
+				secondBrother.Clear();
+
+				for (int i = 0; i < selected.Length; i++)
+				{
+					if (selected[i])
+					{
+						firstBrother.Add(values[i]);
+					}
+					else
+					{
+						secondBrother.Add(values[i]);
+					}
+				}
+			}
+			return;
+		}
+
+		// Try adding the item to the first brother's share
+		selected[index] = true;
+		CanDivide(index + 1, sum1 + values[index], sum2);
+
+		// Try adding the item to the second brother's share
+		selected[index] = false;
+		CanDivide(index + 1, sum1, sum2 + values[index]);
+		stopwatch.Stop();
+		Console.WriteLine($"Function working time: {stopwatch.Elapsed}");
+		Console.WriteLine();
+	}
+
+	static void Main()
+	{
+		values = new int[] { 2, 4, 5, 3 };
+		selected = new bool[values.Length];
+		firstBrother = new List<int>();
+		secondBrother = new List<int>();
+
+		CanDivide(0, 0, 0);
+
+		if (minDifference == 0)
+		{
+			Console.WriteLine("The treasure can be divided equally:");
+		}
+		else
+		{
+			Console.WriteLine("The treasure cannot be divided equally, but can be divided similarly:");
+		}
+		Console.WriteLine("First brother's share: " + string.Join(", ", firstBrother));
+		Console.WriteLine("Second brother's share: " + string.Join(", ", secondBrother));
+	}
+}
+
+//Dynamic
+//using System;
+
+//public class TreasureDivision
+//{
+//	public static bool CanDivide(int[] items)
+//	{
+//		int n = items.Length;
+//		int S = 0;
+//		for (int i = 0; i < n; i++)
+//		{
+//			S += items[i];
+//		}
+//		if (S % 2 != 0)
+//		{
+//			// The total sum is odd, so the items cannot be divided into two groups with equal sums.
+//			return false;
+//		}
+//		bool[,] DP = new bool[n + 1, S / 2 + 1];
+//		// Initialize the base case where the sum is zero.
+//		for (int i = 0; i <= n; i++)
+//		{
+//			DP[i, 0] = true;
+//		}
+//		// Compute the remaining cases.
+//		for (int i = 1; i <= n; i++)
+//		{
+//			for (int j = 1; j <= S / 2; j++)
+//			{
+//				if (items[i - 1] > j)
+//				{
+//					DP[i, j] = DP[i - 1, j];
+//				}
+//				else
+//				{
+//					DP[i, j] = DP[i - 1, j] || DP[i - 1, j - items[i - 1]];
+//				}
+//			}
+//		}
+//		return DP[n, S / 2];
+//	}
+
+//	public static void Main(string[] args)
+//	{
+//		int[] items = new int[] { 2, 4, 5, 6, 7 };
+//		if (CanDivide(items))
+//		{
+//			Console.WriteLine("The items can be divided into two groups with equal sums.");
+//		}
+//		else
+//		{
+//			Console.WriteLine("The items cannot be divided into two groups with equal sums.");
+//		}
+//	}
+//}
---
+++ Antras laboratorinis/Pirma dalis/PVZ/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace Pirma
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int size = 2000;
+			int n = 10;
+			long k;
+			Console.WriteLine("Pirma rekurentine: ");
+			Console.WriteLine("1 - arr[0] = 0");
+			for (int i = 0; i < n; i++)
+			{
+				Stopwatch stopwatch = new Stopwatch();
+				int[] A = new int[size];
+				A[0] = 0;
+				stopwatch.Start();
+				k = methodToAnalysis1(A);
+				stopwatch.Stop();
+				Console.WriteLine($"Function working time: {stopwatch.Elapsed}");
+				Console.WriteLine($"Elements amount: {size}");
+				Console.WriteLine($"Counter: {k}");
+				Console.WriteLine();
+				GC.Collect();
+				size += 2000;
+			}
+
+			size = 20;
+			Console.WriteLine("Pirma rekurentine: ");
+			Console.WriteLine("1 - arr[0] = 1");
+			for (int i = 0; i < n; i++)
+			{
+				Stopwatch stopwatch = new Stopwatch();
+				int[] A = new int[size];
+				A[0] = -1;
+				stopwatch.Start();
+				k = methodToAnalysis1(A);
+				stopwatch.Stop();
+				Console.WriteLine($"Function working time: {stopwatch.Elapsed}");
+				Console.WriteLine($"Elements amount: {size}");
+				Console.WriteLine($"Counter: {k}");
+				Console.WriteLine();
+				GC.Collect();
+				size += 20;
+			}
+
+			//n = 8;
+			//size = 2000;
+			//Console.WriteLine("Antra rekurentine:");
+			//for (int i = 0; i < n; i++)
+			//{
+			//	Stopwatch stopwatch = new Stopwatch();
+			//	stopwatch.Start();
+			//	k = methodToAnalysis2(size);
+			//	stopwatch.Stop();
+			//	Console.WriteLine($"Function working time: {stopwatch.Elapsed}");
+			//	Console.WriteLine($"Elements amount: {size}");
+			//	Console.WriteLine($"Counter: {k}");
+			//	Console.WriteLine();
+			//	GC.Collect();
+			//	size += 2000;
+			//}
+		}
+
+		//Pirma rekurentine
+		public static long methodToAnalysis1(int[] arr)
+		{
+			long n = arr.Length;
+			long k = n;
+
+			if (arr[0] < 0)
+			{
+				for (int i = 0; i < n; i++)
+				{
+					if (i > 0)
+					{
+						for (int j = 0; j < n; j++)
+						{
+							k -= 2;
+						}
+					}
+				}
+			}
+			return k;
+		}
+
+		//Antra rekurentine
+		public static long methodToAnalysis2(int n)
+
+		{
+
+			long k = 0;
+			int[] arr = new int[n];
+			Random randNum = new Random();
+			for (int i = 0; i < n; i++)
+			{
+				arr[i] = randNum.Next(0, n);
+				k += arr[i] + FF1(i);
+			}
+			return k;
+
+		}
+
+		public static long FF1(int n)
+
+		{
+			if (n > 0)
+			{
+				return FF1(n - 1);
+			}
+			return n;
+		}
+	}
+}
